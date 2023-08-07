ce91c2e3398d9d630110c97dc9529592c476e4b6
alpha <pramadeishsm@gmail.com>
2023-08-07 15:29:54 +0530

    added routing and Menu

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+import Menu from './Menu'
+
+const Navbar = () => {
+  return (
+    <div className='h-12 text-red-500 p-4 flex justify-between border-b-2 border-b-red-500 uppercase'>
+      <div className='text-xl'>
+        <Link href="/">SMP-RES</Link>
+      </div>
+      <div>
+        <Menu/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
